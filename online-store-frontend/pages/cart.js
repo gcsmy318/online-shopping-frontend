4b6bddb74b342c764ef77db83ca9ec4eb54ada51
Chatchapon Silarak <chatchaponsil@gosoft.co.th>
2025-03-07 12:08:11 +0700

    Update Images

--- online-store-frontend/pages/cart.js
+++ online-store-frontend/pages/cart.js
@@ -24,8 +24,10 @@ export default function CartPage() {
     const orderData = { name, address, phone, cart, totalPrice };
 
     try {
-      const res = await fetch('https://online-shopping-backend-production.up.railway.app/api/order', {
+     // const res = await fetch('https://online-shopping-backend-production.up.railway.app/api/order', {
+        const res = await fetch('http://localhost:8080/api/order', {
         method: 'POST',
+           }
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(orderData)
       });
